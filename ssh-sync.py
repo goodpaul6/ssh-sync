c511ae9ec5a9ab95cb26f8d51f978bb738c44276
Apaar Madan <apaar_madan@hotmail.com>
2017-11-06 15:29:28 -0500

    Some changes

--- ssh-sync.py
+++ ssh-sync.py
@@ -1,7 +1,7 @@
 import time, os
-from paramiko import SSHClient
+from paramiko import SSHClient, ssh_exception
 import argparse, scp, getpass
-from scp import SCPClient
+from scp import SCPClient, SCPException
 
 scp = None
 
@@ -11,6 +11,18 @@ def copy_to_remote(local_path, remote_path):
 def copy_from_remote(local_path, remote_path):
     scp.get(remote_path, local_path=local_path, preserve_times=True, recursive=True)
 
+def get_mod_times(dir_path):
+    times = {}
+
+    for dirname, subdirnames, filenames in os.walk(dir_path):
+        for filename in filenames:
+            path = os.path.join(dirname, filename)
+            mod_time = os.path.getmtime(path)
+
+            times[path] = mod_time
+
+    return times
+
 def main():
     parser = argparse.ArgumentParser(description="Tool for synchronizing changes between local filesystem and remote shell")
 
@@ -19,6 +31,9 @@ def main():
     parser.add_argument("-w", "--wait-time", type=int, default=5, help="Number of seconds between each sync.")
     parser.add_argument("-d", "--remote-dir", type=str, required=True, help="Remote path to directory to sync (MUST be absolute).")
     parser.add_argument("-c", "--copy", action="store_true", help="Copies remote directory to current directory.")
+    parser.add_argument("-t", "--transfer", action="store_true", help="Copies local directory to remote.")
+    parser.add_argument("--assume-sync", action="store_true", help="Do not force synchronization at startup.")
+    parser.add_argument("-k", "--keep-alive", action="store_true", help="Attempt to reconnect if SSH session is forcibly closed by remote host.")
     parser.add_argument("-v", "--verbose", action="store_true", help="Print verbose output.")
 
     args = parser.parse_args()
@@ -43,16 +58,30 @@ def main():
     remote_dir_dir = os.path.dirname(args.remote_dir)
     local_dir = os.path.basename(args.remote_dir)
 
+    if args.copy and args.remote:
+        print("ERROR: Cannot --copy and --transfer. Pick one.")
+        return
+    
     if args.copy:
         print("Copying directory from remote...")
         copy_from_remote(".", args.remote_dir)
         print("Successfully copied directory from remote.")
+    elif args.transfer:
+        print("Copying directory to remote...")
+        copy_to_remote(local_dir, args.remote_dir)
+        print("Successfully copied directory to remote.")
+    else:
+        if not args.assume_sync:
+            print("ERROR: Must either copy remote directory or transfer local directory on startup. Specify '-c' or '-t' respectively. Alternatively, specify '--assume-sync'.")
+            return
+        else:
+            print("WARNING: Assuming local and remote directories are synchronized.")
 
     if not os.path.exists(local_dir):
         print("ERROR: No local copy exists. Invoke with '-c'.".format(local_dir))
         return
 
-    prev_times = {}
+    prev_times = get_mod_times(local_dir)
 
     def run_command_file(path, dirname):
         base_cmd = "cd {}/{} && ".format(remote_dir_dir, dirname.replace('\\', '/'))
@@ -75,36 +104,72 @@ def main():
 
                 print("Execution finished.")
 
-    while True:
-        if args.verbose:
-            print("Polling for changes...")
+    def loop():
+        while True:
+            if args.verbose:
+                print("Polling for changes...")
 
-        for dirname, subdirnames, filenames in os.walk(local_dir):
-            for filename in filenames:
-                if filename.endswith(".swp"): continue
+            cur_times = get_mod_times(local_dir)
+            
+            nonlocal prev_times
 
-                path = os.path.join(dirname, filename)
-                mod_time = os.path.getmtime(path) 
+            # Check for deleted files
+            for path in prev_times:
+                if not path in cur_times:
+                    remote_path = "{}/{}".format(remote_dir_dir, path.replace("\\", "/"))
+
+                    print("Detected file '{}' was deleted, deleting on remote machine...")
+                    _, _, stderr = ssh.exec_command("rm {}".format(remote_path))
+
+                    lines = list(stderr.readlines())
+                    if len(lines):
+                        print("ERROR:")
+                        for line in lines:
+                            print(line, end='')
+                    else:
+                        print("Successfully deleted file on remote machine.")
+
+            # Synchronize and add files
+            for path in cur_times:
+                dirname = os.path.dirname(path)
+                filename = os.path.basename(path)
+                remote_path = "{}/{}".format(remote_dir_dir, path.replace("\\", "/"))
+
+                if filename.endswith(".swp") or filename.endswith(".swm"): continue
+
                 if path in prev_times:
-                    if mod_time > prev_times[path]:
+                    if prev_times[path] < cur_times[path]:
                         if filename == ".command":
                             print("Detected change in command file in '{}'".format(dirname))
                             run_command_file(path, dirname)
-                        else: 
+                        else:
                             print("Detected change in '{}', copying to remote...".format(path))
-                            copy_to_remote(path, "{}/{}/{}".format(remote_dir_dir, dirname.replace("\\", "/"), filename))
+                            copy_to_remote(path, remote_path)
                             print("Successfully copied to remote.")
-                        prev_times[path] = mod_time
                 else:
                     if filename == ".command":
                         print("Detected  command file in '{}'".format(dirname))
                         print("Modify this to run it.")
+                    else:
+                        print("Detected new file '{}', copying to remote...".format(path))
+                        copy_to_remote(path, remote_path)
+                        print("Successfully copied to remote.")
 
-                    prev_times[path] = mod_time
-        
-        if args.verbose:
-            print("Waiting...")
-        time.sleep(args.wait_time)
+            prev_times = cur_times
+
+            if args.verbose:
+                print("Waiting...")
+            time.sleep(args.wait_time)
+
+    try:
+        loop()
+    except (ssh_exception.SSHException, SCPException, ConnectionResetError):
+        if args.keep_alive:
+            print("Encountered SSH Error")
+            print("Attempting to reconnect...")
+            ssh.connect(args.remote_domain, username=args.user_id, password=args.password)
+            print("Successfully reconnected.")
+            loop()
 
     scp.close()
 
